213a4b335300c4269ad5becba963e99f75b1f909
VIVEK VERMA <119332108+verma2209@users.noreply.github.com>
2024-07-18 15:18:02 +0530

    Update FirebaseConfig.jsx

--- src/firebase/FirebaseConfig.jsx
+++ src/firebase/FirebaseConfig.jsx
@@ -4,14 +4,6 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBmQg4jnRhhA-7Co_u7W9uVYOo--fzGk-E",
-  authDomain: "myecom-1c659.firebaseapp.com",
-  projectId: "myecom-1c659",
-  storageBucket: "myecom-1c659.appspot.com",
-  messagingSenderId: "473490357505",
-  appId: "1:473490357505:web:0048bf613d57a71c9b2f00",
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
